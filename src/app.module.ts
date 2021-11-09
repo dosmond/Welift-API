cf5df2d3b41b58eb59763089f046c192dfe3b391
Daniel Osmond <daniel@thewelift.com>
2021-11-09 15:45:17 -0700

    Added modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,42 @@ import { AppService } from './app.service';
 import { configService } from './config/config.service';
 import { AddressModule } from './address/address.module';
 import { AuthModule } from './auth/auth.module';
+import { AcceptedLiftModule } from './accepted-lift/accepted-lift.module';
+import { BadgeModule } from './badge/badge.module';
+import { BookingModule } from './booking/booking.module';
+import { CompletedLifterBadgeModule } from './completed-lifter-badge/completed-lifter-badge.module';
+import { EquipmentModule } from './equipment/equipment.module';
+import { LeadsModule } from './leads/leads.module';
+import { LifterCompletedTrainingVideosModule } from './lifter-completed-training-videos/lifter-completed-training-videos.module';
+import { LifterEquipmentModule } from './lifter-equipment/lifter-equipment.module';
+import { LifterReviewsModule } from './lifter-reviews/lifter-reviews.module';
+import { LiftersModule } from './lifters/lifters.module';
+import { LiftsModule } from './lifts/lifts.module';
+import { PartnerCreditHourPurchasesModule } from './partner-credit-hour-purchases/partner-credit-hour-purchases.module';
+import { PartnerReferralsModule } from './partner-referrals/partner-referrals.module';
+import { PartnersModule } from './partners/partners.module';
+import { TrainingVideosModule } from './training-videos/training-videos.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     AddressModule,
-    AuthModule
+    AuthModule,
+    AcceptedLiftModule,
+    BadgeModule,
+    BookingModule,
+    CompletedLifterBadgeModule,
+    EquipmentModule,
+    LeadsModule,
+    LifterCompletedTrainingVideosModule,
+    LifterEquipmentModule,
+    LifterReviewsModule,
+    LiftersModule,
+    LiftsModule,
+    PartnerCreditHourPurchasesModule,
+    PartnerReferralsModule,
+    PartnersModule,
+    TrainingVideosModule
   ],
   controllers: [AppController],
   providers: [AppService, RolesGuard],
