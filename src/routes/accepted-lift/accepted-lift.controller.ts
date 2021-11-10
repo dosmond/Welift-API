ba709086916fb932426eacde5a271894e5c85820
Daniel Osmond <daniel@thewelift.com>
2021-11-10 13:09:00 -0700

    Added AcceptedLiftDTO

--- src/routes/accepted-lift/accepted-lift.controller.ts
+++ src/routes/accepted-lift/accepted-lift.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { AcceptedLiftDTO } from 'src/dto/acceptedLift.dto';
 import { AcceptedLift } from 'src/model/acceptedLift.entity';
 import { AcceptedLiftService } from './accepted-lift.service';
 
@@ -7,12 +8,12 @@ export class AcceptedLiftController {
   constructor(private serv: AcceptedLiftService) { }
 
   @Get()
-  public async getAll(@Query() query) {
+  public async getAll(@Query() query): Promise<AcceptedLiftDTO[]> {
     return await this.serv.getAll(query.start, query.end, query.order, query.page, query.pageSize);
   }
 
   @Get('accepted')
-  public async getById(@Query() query): Promise<AcceptedLift> {
+  public async getById(@Query() query): Promise<AcceptedLiftDTO> {
     return await this.serv.getById(query.id);
   }
 }
