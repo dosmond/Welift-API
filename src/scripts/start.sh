5482948cce237047634611c5d8ed3bc78a04bc84
Daniel Osmond <daniel@thewelift.com>
2021-11-22 12:53:54 -0600

    changed start script

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 if [ "$RUN_MIGRATIONS" ]; then
   echo "RUNNING MIGRATIONS";
-  npm run typeorm:migration:run
+  yarn typeorm:migration:run
 fi
 echo "START SERVER";
-npm run start:prod
+yarn start:prod
