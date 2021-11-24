99f6fee70c63a8c592569f5acc3ebdd548360b51
Daniel Osmond <daniel@thewelift.com>
2021-11-24 11:49:19 -0700

    Test

--- src/scripts/write-type-orm-config.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// import { configService } from '../config/config.service';
-const { configService } = require('../config/config.service-module');
-const fs = require('fs');
-fs.writeFileSync(
-  'ormconfig.json',
-  JSON.stringify(configService.getTypeOrmConfig(), null, 2),
-);
