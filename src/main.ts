42e267bbd7757137f736ecd89654892122a0d5ae
Daniel Osmond <daniel@thewelift.com>
2021-12-23 12:48:52 -0700

    extended true

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.use(express.json({ limit: '50mb' }));
-  app.use(express.urlencoded({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(3000);
 }
 
