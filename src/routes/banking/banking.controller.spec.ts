f87df357067ff3e8bfea7ecef4df5692e59414e7
Daniel Osmond <daniel@thewelift.com>
2022-02-03 14:38:43 -0700

    Fixed banking tests

--- src/routes/banking/banking.controller.spec.ts
+++ src/routes/banking/banking.controller.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from '@src/config/config.service';
+import { Lifter } from '@src/model/lifters.entity';
 import { BankingController } from './banking.controller';
 import { BankingService } from './banking.service';
 
@@ -7,6 +10,10 @@ describe('BankingController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+        TypeOrmModule.forFeature([Lifter]),
+      ],
       controllers: [BankingController],
       providers: [BankingService],
     }).compile();
--- src/routes/banking/banking.service.spec.ts
+++ src/routes/banking/banking.service.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from '@src/config/config.service';
+import { Lifter } from '@src/model/lifters.entity';
 import { BankingService } from './banking.service';
 
 describe('BankingService', () => {
@@ -7,6 +10,10 @@ describe('BankingService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [BankingService],
+      imports: [
+        TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+        TypeOrmModule.forFeature([Lifter]),
+      ],
     }).compile();
 
     service = module.get<BankingService>(BankingService);
