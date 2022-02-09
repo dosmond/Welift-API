60124ad566b6e77e8a09ede30d9797e1fedbb7d5
Daniel Osmond <daniel@thewelift.com>
2022-02-09 16:53:28 -0700

    Remove package

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import * as express from 'express';
 import 'module-alias/register';
 import { Logger } from 'nestjs-pino';
 import { ErrorLoggerInterceptor } from './interceptors/error-logger.interceptor';
-import * as requestIp from 'request-ip';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
@@ -15,7 +14,6 @@ async function bootstrap() {
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
   app.useLogger(app.get(Logger));
   app.useGlobalInterceptors(new ErrorLoggerInterceptor());
-  app.use(requestIp.mw());
   await app.listen(3000);
 }
 
