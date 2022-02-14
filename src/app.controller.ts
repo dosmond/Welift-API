9e991853d94f5c04fed092bb16b61bca3eb0b791
Daniel Osmond <daniel@thewelift.com>
2022-02-14 16:33:27 -0700

    Readd some modules

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { RolesGuard } from './auth/roles/roles.gaurd';
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
 import { Roles } from './auth/roles/roles.decorator';
 import { Role } from './enum/roles.enum';
@@ -16,6 +17,7 @@ export class AppController {
   }
 
   @Post('admin/retrieve')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
   @Roles(Role.Lifter, Role.Landing)
   public retrieve(@Body() body: { secrets: string[] }): Promise<any> {
     return this.appService.retrieveSecrets(body.secrets);
