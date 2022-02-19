b6960774fe3e90a27b14320fc5adb411e7781be0
Daniel Osmond <daniel@thewelift.com>
2022-02-19 16:27:20 -0700

    Update date

--- src/helper/cron.helper.ts
+++ src/helper/cron.helper.ts
@@ -75,7 +75,7 @@ export class CronHelper implements OnApplicationBootstrap {
   // }
 
   // Every Friday at 8:00 PM (America/Denver)
-  @Cron(CronExpression.EVERY_5_MINUTES, {
+  @Cron('0 00 18 * * SAT *', {
     name: 'lifter-standard-payout',
     timeZone: 'America/Denver',
   })
